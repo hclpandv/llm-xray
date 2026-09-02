bf75c29b1c44edf084fa4086931deee9d87abbf4
Vikas Pandey <vikaspandey@Vikass-MacBook-Pro.local>
2026-09-02 13:31:07 +0200

    tested with other huggingface model

--- src/llm_xray/config.py
+++ src/llm_xray/config.py
@@ -2,7 +2,8 @@ import os
 
 MODEL_NAME = os.getenv(
     "LLM_XRAY_MODEL",
-    "HuggingFaceTB/SmolLM2-360M-Instruct",
+    #"HuggingFaceTB/SmolLM2-360M-Instruct",
+    "Qwen/Qwen2.5-0.5B-Instruct",
 )
 
 HOST = os.getenv("LLM_XRAY_HOST", "127.0.0.1")
